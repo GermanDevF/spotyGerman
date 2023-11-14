95d575761b332a33d587acc39b82db7f6bef7c28
GermanDevF <alejandrogermanleon@gmail.com>
2023-11-13 22:53:30 -0700

    first commit

---
+++ src/components/Greatings.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours()
+
+  let greating=""
+  if (currentHour<12) {
+    greating = "Buenos días"
+  }else if(currentHour<18) {
+    greating = "Buenas tardes"
+  }else{
+    greating= "Buenas noches"
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greating}</h1>
